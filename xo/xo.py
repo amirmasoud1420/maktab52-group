9c3c3715bbc43c8ecd754e9e8ee95e554a2ac2b2
unknown <amir.talebnia@gmail.com>
2021-05-03 03:10:32 +0430

    finished mark v.1

--- xo/xo.py
+++ xo/xo.py
@@ -4,7 +4,7 @@ from typing import Literal, Union, Optional
 class _Player:
     def __init__(self, name: str, sign: Literal['x', 'o']) -> None:
         self.name = name
-        self.sign = sign
+        self.sign = str(sign).lower()
 
 
 class _XOTable:
@@ -31,21 +31,24 @@ class _XOTable:
         self.xo_map[cell_no] = sign
 
 
+class FinishedGameError(Exception):
+    "mark: dar zamin k bazi tamoom shde ..."
+    pass
+
+
+class InvalidCellError(Exception):
+    "mark: Che por bashe, che addesh eshtabah bashe va ..."
+    pass
+
+
+class InvalidPlayer(Exception):
+    "mark: palyere voroodi eshtebah bashad!!!"
+    pass
+
+
 class _XOGame(_XOTable):
     class UnFinishedGameError(Exception):
         "winner: zamani raise mishe k, bazi tamoom nashode bahe, vali winner() ..."
-        pass
-
-    class FinishedGameError(Exception):
-        "mark: dar zamin k bazi tamoom shde ..."
-        pass
-
-    class InvalidCellError(Exception):
-        "mark: Che por bashe, che addesh eshtabah bashe va ..."
-        pass
-
-    class InvalidPlayer(Exception):
-        "mark: palyere voroodi eshtebah bashad!!!"
         pass
 
     def __init__(self, player1: _Player, player2: _Player) -> None:
@@ -57,7 +60,25 @@ class _XOGame(_XOTable):
         pass
 
     def mark(self, cell_no, player: Union[_Player, Literal['x', 'o'], int]):
-        pass
+        if self._calculate_result():
+            raise FinishedGameError('The Game is Finished!!!')
+        if not isinstance(cell_no, int):
+            raise InvalidCellError('Invalid cell number!!!')
+        if not (1 <= cell_no <= 9):
+            raise InvalidCellError('Invalid cell number!!!')
+        if self.xo_map[cell_no]:
+            raise InvalidCellError('Invalid cell number!!!')
+        if not (str(player) in 'xoXO' or player == 1 or player == 2 or isinstance(player, _Player)):
+            raise InvalidPlayer("Invalid player")
+        if player == 1 or player == 'x' or player == 'X':
+            super(_XOGame, self).mark(cell_no, 'x')
+        elif player == 2 or player == 'o' or player == 'O':
+            super(_XOGame, self).mark(cell_no, 'o')
+        elif isinstance(player, _Player):
+            if player.sign == 'x':
+                super(_XOGame, self).mark(cell_no, 'x')
+            elif player.sign == 'o':
+                super(_XOGame, self).mark(cell_no, 'o')
 
     def winner(self) -> Optional[_Player]:
         pass
